87751684a9dbe123b0b717d8104a5d4d38704cd6
TraianAlex <victor_traian@yahoo.com>
2021-02-23 21:22:36 -0500

    comments, post, hooks

--- src/components/sidebar/index.js
+++ src/components/sidebar/index.js
@@ -1,5 +1,5 @@
 import React from 'react';
-import useUser from '../../hooks/use-user';
+// import useUser from '../../hooks/use-user';
 
 export default function Sidebar() {
     // const { user: { docId, userId, following, username, fullName } = {} } = useUser();
--- src/components/timeline.js
+++ src/components/timeline.js
@@ -1,5 +1,26 @@
 import React from 'react';
+import Skeleton from 'react-loading-skeleton';
+import Post from './post';
+import useFollowedUsersPhotos from '../hooks/use-followed-users-photos';
 
 export default function Timeline() {
-    return (<p>I am the timeline</p>);
+    const { photos } = useFollowedUsersPhotos();
+
+    return (
+        <div className="container col-span-2">
+            {!photos ? (
+                <>
+                    {[...new Array(4)].map((_, index) => (
+                        <Skeleton key={index} count={1} width={320} height={400} />
+                    ))}
+                </>
+            ) : photos && photos.length > 0 ? (
+                photos.map((content) => <Post key={content.docId} content={content} />)
+            ) : (
+                        <p className="text-center text-2xl">Follow people to see photos!</p>
+                    )}
+        </div>
+    );
 }
+
+//   - https://www.npmjs.com/package/react-loading-skeleton
