f51b940db069fe9bb9ccc1d78c75403b141786c9
Traian Alexandru <victor_traian@yahoo.com>
2021-02-26 00:51:29 -0500

    helpers

--- src/App.js
+++ src/App.js
@@ -1,6 +1,8 @@
 import React, { lazy, Suspense } from 'react';
 import { BrowserRouter as Router, Route, Switch } from 'react-router-dom';
 
+import ProtectedRoute from './helpers/protected-route';
+import IsUserLoggedIn from './helpers/is-user-logged-in';
 import UserContext from './context/user';
 import useAuthListener from './hooks/use-auth-listener';
 import * as ROUTES from './constants/routes';
@@ -11,24 +13,36 @@ const SignUp = lazy(() => import('./pages/signup'));
 const Profile = lazy(() => import('./pages/profile'));
 const NotFound = lazy(() => import('./pages/not-found'));
 
-function App() {
+export default function App() {
   const { user } = useAuthListener();
 
   return (
-      <UserContext.Provider value={{ user }}>
-        <Router>
-          <Suspense fallback={<p>Loading...</p>}>
-            <Switch>
-              <Route path={ROUTES.LOGIN} component={Login} />
-              <Route path={ROUTES.SIGN_UP} component={SignUp} />
-              <Route path={ROUTES.PROFILE} component={Profile} />
-              <Route path={ROUTES.DASHBOARD} component={Dashboard} exact />
-              <Route component={NotFound} />
-            </Switch>
-          </Suspense >
-        </Router>
-      </UserContext.Provider>
+    <UserContext.Provider value={{ user }}>
+      <Router>
+        <Suspense fallback={<p>Loading...</p>}>
+          <Switch>
+            <IsUserLoggedIn
+              user={user}
+              loggedInPath={ROUTES.DASHBOARD}
+              path={ROUTES.LOGIN}
+            >
+              <Login />
+            </IsUserLoggedIn>
+            <IsUserLoggedIn
+              user={user}
+              loggedInPath={ROUTES.DASHBOARD}
+              path={ROUTES.SIGN_UP}
+            >
+              <SignUp />
+            </IsUserLoggedIn>
+            <Route path={ROUTES.PROFILE} component={Profile} />
+            <ProtectedRoute user={user} path={ROUTES.DASHBOARD} exact>
+              <Dashboard />
+            </ProtectedRoute>
+            <Route component={NotFound} />
+          </Switch>
+        </Suspense>
+      </Router>
+    </UserContext.Provider>
   );
 }
-
-export default App;
--- src/hooks/use-followed-users-photos.js
+++ src/hooks/use-followed-users-photos.js
@@ -1,27 +1,31 @@
+// @ts-nocheck
 import { useState, useEffect, useContext } from 'react';
 import { getUserByUserId, getUserFollowedPhotos } from '../services/firebase';
 import UserContext from '../context/user';
 
 export default function useFollowedUsersPhotos() {
-    const [photos, setPhotos] = useState(null);
-    const {
-        user: { uid: userId = '' }
-    } = useContext(UserContext);
-    
-    useEffect(() => {
-        async function getTimelinePhotos() {
-            const followingUserIds = await getUserByUserId(userId);
+  const [photos, setPhotos] = useState(null);
+  const {
+    user: { uid: userId = '' },
+  } = useContext(UserContext);
 
-            if (followingUserIds && followingUserIds[0].following.length > 0) {
-                const followedUserPhotos = await getUserFollowedPhotos(userId, followingUserIds[0].following);
-                
-                followedUserPhotos.sort((a, b) => b.dateCreated - a.dateCreated);
-                setPhotos(followedUserPhotos);
-            }
-         }
-        
-        getTimelinePhotos();
-    }, [userId]);
-    
-    return { photos };
+  useEffect(() => {
+    async function getTimelinePhotos() {
+      const followingUserIds = await getUserByUserId(userId);
+
+      if (followingUserIds && followingUserIds[0].following.length > 0) {
+        const followedUserPhotos = await getUserFollowedPhotos(
+          userId,
+          followingUserIds[0].following,
+        );
+
+        followedUserPhotos.sort((a, b) => b.dateCreated - a.dateCreated);
+        setPhotos(followedUserPhotos);
+      }
+    }
+
+    getTimelinePhotos();
+  }, [userId]);
+
+  return { photos };
 }
