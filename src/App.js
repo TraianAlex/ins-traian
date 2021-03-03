282876eb24aa8dfc40077e290cf2f4580711e48a
Traian Alexandru <victor_traian@yahoo.com>
2021-03-02 23:19:53 -0500

    clean

--- src/App.js
+++ src/App.js
@@ -47,21 +47,6 @@ export default function App() {
   );
 }
 
-// import React, { lazy, Suspense } from 'react';
-// import { BrowserRouter as Router, Route, Switch } from 'react-router-dom';
-
-// import ProtectedRoute from './helpers/protected-route';
-// import IsUserLoggedIn from './helpers/is-user-logged-in';
-// import UserContext from './context/user';
-// import useAuthListener from './hooks/use-auth-listener';
-// import * as ROUTES from './constants/routes';
-
-// const Dashboard = lazy(() => import('./pages/dashboard'));
-// const Login = lazy(() => import('./pages/login'));
-// const SignUp = lazy(() => import('./pages/signup'));
-// const Profile = lazy(() => import('./pages/profile'));
-// const NotFound = lazy(() => import('./pages/not-found'));
-
 // export default function App() {
 //   const { user } = useAuthListener();
 
